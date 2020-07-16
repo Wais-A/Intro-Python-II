7319a169704da204c12b6007950764ba28db0196
Wais-A <wais.almakaleh@gmail.com>
2020-07-16 17:45:17 -0400

    a little bit confused

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,23 @@
 from room import Room
-
+from player import Player
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room("Foyer",
+                     """Dim light filters in from the south. Dusty passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'overlook': Room("Grand Overlook",
+                     """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+                    to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+                    chamber! Sadly, it has already been completely emptied by
+                    earlier adventurers. The only exit is to the south."""),
 }
 
 
@@ -36,9 +35,8 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
-
 # Make a new player object that is currently in the 'outside' room.
-
+new_player = Player('Wais', room['outside'])
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +47,39 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+while True:
+    current_room = new_player.current_room
+    print(
+        f'You are located at {current_room.name} located at {current_room.description}')
+    print("where are you going")
+    answer = input('Enter n, s , e, w and q to quit the game: ')
+
+    if answer == 'n':
+        if hasattr(current_room, 'n_to'):
+            print('Head North')
+            new_player.current_room = current_room.n_to
+        else:
+            print(f'{current_room.name} enter another location')
+    elif answer == 's':
+        if hasattr(current_room, 's_to'):
+            print('Head south!')
+            new_player.current_room = current_room.s_to
+        else:
+            print(f' {current_room.name} enter another location')
+    elif answer == 'e':
+        if hasattr(current_room, 'e_to'):
+            print('Head East')
+            new_player.current_room = current_room.e_to
+        else:
+            print(
+                f' {current_room.name}')
+    elif answer == 'w':
+        if hasattr(current_room, 'w_to'):
+            print('Head West')
+            new_player.current_room = current_room.w_to
+        else:
+            print(
+                f' {current_room.name}')
+    elif answer == 'q':
+        print(f'{current_room.name}')
+        exit()
